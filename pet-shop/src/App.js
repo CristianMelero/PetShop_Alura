ff8c1f7cc55055edd521e8e568b24faf3bf95ec0
Cristian Melero <cristianmelero22@yahoo.com.ar>
2023-05-06 18:54:21 -0300

    Clase 1 Completada

--- pet-shop/src/App.js
+++ pet-shop/src/App.js
@@ -1,12 +1,21 @@
-import './assets/css/base/base.css';
-import './assets/css/componentes/card.css'
+import "./assets/css/base/base.css";
+import "./assets/css/componentes/card.css";
+import Home from "./pages/Home";
+import Sobre from "./pages/Sobre";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <>
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/sobre" element={<Sobre />} />
 
-    </>
-  );
+
+      <Route path="*" element={<h1> La página no existe </h1>}/> 
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
